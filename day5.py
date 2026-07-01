5f6579857773280e368422cd93ee05313968cc2f
kritipuri70 <purik6828@gmail.com>
2026-07-01 21:05:05 +0545

    List

--- day5.py
+++ day5.py
@@ -3,5 +3,7 @@ humidity=int(input("Enter humidity   "))
 if(temp>=50):
     if(humidity<=25):
         print("There is a risk of fire")
+    else:
+        print("There is no risk of fire")
 else:
     print("There is no risk of fire")
---
+++ day7.py
@@ -0,0 +1,5 @@
+temp=[50,60,70,80,90]
+th_temp=50
+for i in temp:
+    if(i==th_temp):
+        print("It is too hot")
